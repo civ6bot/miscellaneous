ff24bc946b7801489a0e7b92f3e668d71a7f9ea5
ZhenjaMax <zhenja6@mail.ru>
2023-02-08 01:19:44 +0300

    1.1.2
    - fix slash command access error;
    - fix /about command
    - config empty value fix

--- src/modules/discord/discord.ui.ts
+++ src/modules/discord/discord.ui.ts
@@ -1,20 +1,22 @@
-import { EmbedBuilder } from "discord.js";
+import { ColorResolvable, EmbedBuilder } from "discord.js";
 import { UtilsGeneratorEmbed } from "../../utils/generators/utils.generator.embed";
 import { ModuleBaseUI } from "../base/base.ui";
 
 export class DiscordUI extends ModuleBaseUI {
     public onGuildCreate(
         title: string,
-        description: string
+        color: string,
+        description: string,
+        thumbnailImageURL: string
     ): EmbedBuilder[] {
         return UtilsGeneratorEmbed.getSingle(
             title,
-            "#2EA500",
+            color as ColorResolvable,
             description,
             [],
             null,
             null,
-            "https://media.discordapp.net/attachments/795265098159357953/1048989944154161183/misc.png?width=494&height=494"
+            thumbnailImageURL
         );
     }
 }
